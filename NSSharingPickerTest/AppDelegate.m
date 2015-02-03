35732a2bca822d9eaa903f830d7019b03ab846c9
Ilja Iwas <ilja@iwascoding.com>
2015-02-03 17:02:35 +0100

    initial commit

--- NSSharingPickerTest/AppDelegate.m
+++ NSSharingPickerTest/AppDelegate.m
@@ -10,17 +10,57 @@
 
 @interface AppDelegate ()
 
-@property (weak) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSWindow		*window;
+@property (weak) IBOutlet NSButton		*shareButton;
+@property (weak) IBOutlet NSImageView	*imageView;
+
 @end
 
 @implementation AppDelegate
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-	// Insert code here to initialize your application
+- (IBAction)testSharingPicker:(id)sender
+{
+	NSSharingServicePicker *picker = [[NSSharingServicePicker alloc] initWithItems:@[[self.imageView image]]];
+	
+	[picker setValue:@(1) forKey:@"style"];
+	[picker setDelegate:self];
+	
+	[picker showRelativeToRect:[sender bounds] ofView:sender preferredEdge:NSMinYEdge];
 }
 
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-	// Insert code here to tear down your application
+- (NSArray *)sharingServicePicker:(NSSharingServicePicker *)sharingServicePicker sharingServicesForItems:(NSArray *)items proposedSharingServices:(NSArray *)proposedServices
+{
+	return proposedServices;
+}
+
+- (id<NSSharingServiceDelegate>)sharingServicePicker:(NSSharingServicePicker *)sharingServicePicker delegateForSharingService:(NSSharingService *)sharingService
+{
+	return self;
+}
+
+- (NSRect)sharingService:(NSSharingService *)sharingService sourceFrameOnScreenForShareItem:(id <NSPasteboardWriting>)item
+{
+	if ([item isKindOfClass:[NSImage class]]) {
+		NSRect frame = [self.imageView bounds];
+		frame = [self.imageView convertRect:frame toView:nil];
+		frame = [[self.imageView window] convertRectToScreen:frame];
+		return frame;
+	}
+	return NSZeroRect;
+}
+
+- (NSWindow*) sharingService:(NSSharingService *)sharingService sourceWindowForShareItems:(NSArray *)items sharingContentScope:(NSSharingContentScope *)sharingContentScope
+{
+	return self.imageView.window;
+}
+
+- (NSImage*) sharingService:(NSSharingService *)sharingService transitionImageForShareItem:(id<NSPasteboardWriting>)item contentRect:(NSRect *)contentRect	{
+	return [self.imageView image];
+}
+
+- (BOOL)sharingServicePicker:(NSSharingServicePicker *)sharingService shouldShowForView:(NSView*) inView
+{
+	return YES;
 }
 
 @end
